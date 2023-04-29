a620d5114d36ab08237770195a3d86a3716ca5a3
nelvisstiphen <nelvissalazar.31@gmail.com>
2023-04-28 19:09:01 -0500

    prueba

---
+++ main.js
@@ -0,0 +1,49 @@
+
+
+/*
+const array = ["0", "0", "0", "0", "0", "0"];
+
+const num = 1;
+const decre = 5;
+
+ function repeticion(i, num){
+    if(i == 0){
+        array.splice(decre, 1, num);
+        document.write(array + "<br>")
+    } else if (i == 1){
+        array.splice(4, 2, "1", "0")
+        document.write(array + "<br>")
+    }else if (i == 2){
+        array.splice(3, 3, "1", "0", "0")
+        document.write(array + "<br>")
+    }else if (i == 3){
+        array.splice(2, 4, "1", "0", "0", "0")
+        document.write(array + "<br>")
+    }else if (i == 4){
+        array.splice(1, 5, "1", "0", "0", "0", "0")
+        document.write(array + "<br>")
+    }else if (i == 5){
+        array.splice(0, 6, "1", "0", "0", "0", "0", "0")
+        document.write(array + "<br>")
+    }
+ }
+function bucle(){
+    for(let i = 0; i <= array.length; i++){
+    repeticion(i, num);
+    /* document.write(array + "<br>"); 
+}
+}
+bucle();
+
+
+
+
+ for(let año = 1; año <= 100000; año = año + 10){
+    for(let mes = 1; mes <= 12; mes = mes + 1){
+        document.write(mes + "/" + año + " ");
+    }
+    document.write("<br>")
+}
+ */
+
+/* operacion(num1); */
